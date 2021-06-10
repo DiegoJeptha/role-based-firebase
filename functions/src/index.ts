a3de0ae0709d37f30b558b30cb37fb66364366ab
Diego <diegotgjeptha@gmail.com>
2021-06-10 22:38:20 +0200

    express handler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const app = express();
+
+export const api = functions.https.onRequest(app);
+
